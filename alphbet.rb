d4979dd0f46f78b2eea407cee6d62eef7e377e67
Natalia <6568973@gmail.com>
2018-09-28 11:43:29 -0400

    new commit

--- alphbet.rb
+++ alphbet.rb
@@ -1,18 +1,8 @@
-#alphabet = ('A'..'Z').to_a
-#vowels = "AEIOUY"
-#vowels_hash = {}
-
-#(0..vowels.length - 1).each do |vowel|
-	#vowels_hash[vowels[vowel]] = alphabet.index(vowels[vowel]) + 1
-#end
-
-#puts vowels_hash	
-
 alphabet = ('A'..'Z').to_a
 vowels = ["A", "E", "I", "O", "U", "Y"]
  vowels_hash = {}
 vowels.each do |vowel|
-  vowels_hash[vowel] = alphabet.index(vowel)
+  vowels_hash[vowel] = alphabet.index(vowel) + 1 
 end
 
 puts vowels_hash	
